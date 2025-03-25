da357a058560dfffbd58852f9aab654d7eeec525
Uzurch1k <galcenkovlad7@gmail.com>
2025-03-25 16:51:47 +0200

    add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import './parts/lockscroll.js';
 import './parts/mobmenu.js';
+import './parts/navigation.js';
 import './parts/banner.js';
 import './parts/splidecust.js';
 import './parts/splide.js';
