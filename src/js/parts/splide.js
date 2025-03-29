8c02e4c987c2264b15d5e9e1bc5ceec1977717da
Uzurch1k <galcenkovlad7@gmail.com>
2025-03-29 19:20:22 +0200

    fix

--- src/js/parts/splide.js
+++ src/js/parts/splide.js
@@ -1,23 +1,5 @@
 import { initSlider } from './splidecust';
 
-const createSplide = document.querySelector('.create');
-if (createSplide) {
-  initSlider(createSplide, {
-    gap: '1.5rem',
-    perPage: 3,
-    breakpoints: {
-      960: {
-        gap: '1rem',
-        perPage: 2,
-        pagination: true,
-      },
-      768: {
-        perPage: 1,
-      },
-    },
-  });
-}
-
 const qualitiesSplide = document.querySelector('.qualities');
 if (qualitiesSplide) {
   initSlider(qualitiesSplide, {
@@ -46,34 +28,39 @@ if (qualitiesSplide) {
 //   });
 // });
 
-// let elemSliderInstance;
-// const elem = document.querySelector('.elem');
+let createSliderInstance;
+const create = document.querySelector('.create');
 
-// const initElemSlider = () => {
-//   if (elem && !elemSliderInstance) {
-//     elemSliderInstance = initSlider(elem, {
-//       perPage: 2,
-//       breakpoints: {
-//         960: {},
-//         500: {},
-//       },
-//     });
-//   }
-// };
+const initElemSlider = () => {
+  if (create && !createSliderInstance) {
+    createSliderInstance = initSlider(create, {
+      breakpoints: {
+        960: {
+          gap: '1rem',
+          perPage: 2,
+          pagination: true,
+        },
+        768: {
+          perPage: 1,
+        },
+      },
+    });
+  }
+};
 
-// const destroySliders = () => {
-//   if (elemSliderInstance) {
-//     elemSliderInstance.destroy();
-//     elemSliderInstance = null;
-//   }
-// };
+const destroySliders = () => {
+  if (createSliderInstance) {
+    createSliderInstance.destroy();
+    createSliderInstance = null;
+  }
+};
 
-// const checkViewport = () => {
-//   initElemSlider();
-//   if (window.innerWidth > 960) {
-//     destroySliders();
-//   }
-// };
+const checkViewport = () => {
+  initElemSlider();
+  if (window.innerWidth > 960) {
+    destroySliders();
+  }
+};
 
-// window.addEventListener('resize', checkViewport);
-// document.addEventListener('DOMContentLoaded', checkViewport);
+window.addEventListener('resize', checkViewport);
+document.addEventListener('DOMContentLoaded', checkViewport);
