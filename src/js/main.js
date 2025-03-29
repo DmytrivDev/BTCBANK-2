4c79d9341593674109058addf22288f89c3ecb3c
Uzurch1k <galcenkovlad7@gmail.com>
2025-03-29 21:14:22 +0200

    add options

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import './parts/lockscroll.js';
 import './parts/mobmenu.js';
 import './parts/navigation.js';
 import './parts/banner.js';
+import './parts/tabs.js';
 import './parts/splidecust.js';
 import './parts/splide.js';
 import './parts/accordion.js';
