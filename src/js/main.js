743d73a66c58d01f7ba349986f886100aefcf25f
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-04-04 17:51:50 +0300

    adasdasd

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,9 @@ import './parts/tabs.js';
 import './parts/splidecust.js';
 import './parts/splide.js';
 import './parts/accordion.js';
+
+
+
+setTimeout(evt => {
+    document.querySelector('.mobmenu').style.display = 'block';
+}, 500)
